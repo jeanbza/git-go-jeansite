9579e5e89be1c24a6b195d9f52fcac4d42d32be4
jadekler <jadekler@gmail.com>
2014-01-12 16:45:27 -0500

    Added jquer treetable showcase, finishing up issue #11

--- src/showcase/showcase.go
+++ src/showcase/showcase.go
@@ -33,8 +33,45 @@ func GetPage(rw http.ResponseWriter, req *http.Request) {
                 loadEmberWidgetShowcase(rw)
             case "ember_treetable":
                 loadEmberTreetableShowcase(rw)
+            case "jquery_treetable":
+                loadJqueryTreetableShowcase(rw)
         }
     }
+}
+
+func loadJqueryTreetableShowcase(rw http.ResponseWriter) {
+    type ShowCase struct {
+        Additional  template.HTML
+    }
+
+    type Page struct {
+        Title               string
+        CurrentShowcase     string
+        ShowCase            ShowCase
+    }
+
+    filePaths := []string{
+        "resources/showcases/ember_treetable/templates/app.html",
+    }
+
+    var contentString bytes.Buffer
+
+    for _, filePath := range filePaths {
+        // Read the file's contents
+        contentByte, err := ioutil.ReadFile(filePath)
+        common.CheckError(err)
+
+        contentString.WriteString(string(contentByte))
+    }
+
+    contentHTML := template.HTML(contentString.String())
+
+    s := ShowCase{Additional: contentHTML}
+    p := Page{Title: "showcase", CurrentShowcase: "jquery_treetable", ShowCase: s}
+
+    tmpl := make(map[string]*template.Template)
+    tmpl["showcase.html"] = template.Must(template.ParseFiles("resources/html/showcase.html", "resources/html/index.html", "resources/showcases/jquery_treetable/showcase.html"))
+    tmpl["showcase.html"].ExecuteTemplate(rw, "base", p)
 }
 
 func loadEmberTreetableShowcase(rw http.ResponseWriter) {
